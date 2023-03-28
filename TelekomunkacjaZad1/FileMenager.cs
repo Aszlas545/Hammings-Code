e7476209dedc7cdb90d5c45d07905341ded71787
Aszlas545 <127033646+Aszlas545@users.noreply.github.com>
2023-03-28 22:44:30 +0200

    Add files via upload

---
+++ TelekomunkacjaZad1/FileMenager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekomunikacja1
+{
+    internal class FileMenager
+    {
+        //------------------------------------------
+        //Funkcja realizująca odczyt bajtów z pliku
+        //------------------------------------------
+        public byte[] readFile(string path)
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+            return bytes;
+        }
+
+        //-----------------------------------------
+        //Funkcja realizująca zapis bajtów z pliku
+        //-----------------------------------------
+        public void saveFile(byte[] data, string filePath)
+           => File.WriteAllBytes(filePath, data);
+
+    }
+}
